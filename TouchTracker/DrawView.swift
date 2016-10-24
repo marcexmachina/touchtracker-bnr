e7b558a9cfc8e763cfab742787efec5611a53081
Marc <marc.oneill2@gmail.com>
2016-10-24 20:50:40 +1100

    Adding Line Struct and new custom view DrawView

---
+++ TouchTracker/DrawView.swift
@@ -0,0 +1,16 @@
+//
+//  DrawView.swift
+//  TouchTracker
+//
+//  Created by Marc O'Neill on 24/10/2016.
+//  Copyright © 2016 marcondev. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    
+    var currentLine: Line?
+    var finishedLines = [Line]()
+    
+}
---
+++ TouchTracker/Line.swift
@@ -0,0 +1,14 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Marc O'Neill on 24/10/2016.
+//  Copyright © 2016 marcondev. All rights reserved.
+//
+
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
