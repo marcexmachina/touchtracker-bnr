68648453d4a1388a332f4bc9bca5e546cc260e15
Marc <marc.oneill2@gmail.com>
2016-10-24 21:08:42 +1100

    Implemented drawing logic

--- TouchTracker/DrawView.swift
+++ TouchTracker/DrawView.swift
@@ -13,4 +13,67 @@ class DrawView: UIView {
     var currentLine: Line?
     var finishedLines = [Line]()
     
+    func strokeLine(line: Line) {
+        let path = UIBezierPath()
+        path.lineWidth = 10
+        path.lineCapStyle = CGLineCap.Round
+        
+        path.moveToPoint(line.begin)
+        path.addLineToPoint(line.end)
+        path.stroke()
+    }
+    
+    override func drawRect(rect: CGRect) {
+        UIColor.blackColor().setStroke()
+        for line in finishedLines {
+            strokeLine(line)
+        }
+        
+        if let line = currentLine {
+            UIColor.redColor().setStroke()
+            strokeLine(line)
+        }
+    }
+    
+    // MARK: UIResponders
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first!
+        
+        //get location of touch in views coordinate system
+        let location = touch.locationInView(self)
+        currentLine = Line(begin: location, end: location)
+        
+        setNeedsDisplay()
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first!
+        
+        let location = touch.locationInView(self)
+        currentLine?.end = location
+        
+        setNeedsDisplay()
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if var line = currentLine {
+            let touch = touches.first!
+            let location = touch.locationInView(self)
+            line.end = location
+            
+            finishedLines.append(line)
+        }
+        currentLine = nil
+        setNeedsDisplay()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
